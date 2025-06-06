2257f4d6b1608fdcefa9e0f34d718e6ef165fc53
Jinook Jung <jinookjung@Mac.lan>
2025-06-06 09:47:03 -0400

    add comment

--- firstRecurringCharacterIndex.js
+++ firstRecurringCharacterIndex.js
@@ -1,34 +1,36 @@
 function firstRecurringCharacterIndex(str) {
-//   const firstOccurrence = new Array(26);
-//   let result;
-
-//   for (let i = 0; i < str.length; i++) {
-//     const charCode = str.charCodeAt(i) - 'a'.charCodeAt(0);
-
-//     if (firstOccurrence[charCode] === undefined) {
-//       firstOccurrence[charCode] = i;
-//     } else {
-//       result = result === undefined ? firstOccurrence[charCode] : Math.min(result, firstOccurrence[charCode]);
-//     }
-//   }
-//   // console.log(firstOccurrence)
-//   return result;
-  
-  const charArray = new Array(26);
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  //// Record firstOccurrenc index
+  const firstOccurrence = new Array(26);
   let result;
 
   for (let i = 0; i < str.length; i++) {
     const charCode = str.charCodeAt(i) - 'a'.charCodeAt(0);
 
-    if (charArray[charCode] === undefined) {
-      charArray[charCode] = 1;
+    if (firstOccurrence[charCode] === undefined) {
+      firstOccurrence[charCode] = i;
     } else {
-      result = result === undefined ? str.indexOf(alphabet[charCode]) : Math.min(result, str.indexOf(alphabet[charCode]));
+      result = result === undefined ? firstOccurrence[charCode] : Math.min(result, firstOccurrence[charCode]);
     }
   }
-  // console.log(charArray)
+  // console.log(firstOccurrence)
   return result;
+  
+  // //// Use alphabet string to match charArray
+  // const charArray = new Array(26);
+  // const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  // let result;
+
+  // for (let i = 0; i < str.length; i++) {
+  //   const charCode = str.charCodeAt(i) - 'a'.charCodeAt(0);
+
+  //   if (charArray[charCode] === undefined) {
+  //     charArray[charCode] = true;
+  //   } else {
+  //     result = result === undefined ? str.indexOf(alphabet[charCode]) : Math.min(result, str.indexOf(alphabet[charCode]));
+  //   }
+  // }
+  // // console.log(charArray)
+  // return result;
 }
 
 console.log(firstRecurringCharacterIndex("google")); // 0
